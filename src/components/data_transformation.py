d4d3f3ecea6c38e56a5547fe81f010c4cfab18df
Shardul Pingale <shardulpingale02@gmail.com>
2023-07-13 13:45:13 +0530

    Model Trainer, Best Model Evaluation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -40,7 +40,7 @@ class DataTransformation:
             num_pipeline= Pipeline(
                 steps=[
                 ("imputer",SimpleImputer(strategy="median")),
-                ("scaler",StandardScaler())
+                ("scaler",StandardScaler(with_mean=False))
 
                 ]
             )

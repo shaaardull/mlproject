82ac7c64e152e14b8fa5a0419e2daff2979f3b60
Shardul Pingale <shardulpingale02@gmail.com>
2023-07-27 02:44:51 +0530

    Deployment Configuration

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages,setup
-#from typing import List
+from typing import List
 
 
 HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path):
+def get_requirements(file_path:str) -> List(str):
     '''
     This function will return the list of requirements
     '''
